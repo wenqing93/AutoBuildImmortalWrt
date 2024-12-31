b60f72c29ce06766a2a4e0611b4bbc8c94079ae7
wenqing93 <80711860+wenqing93@users.noreply.github.com>
2025-01-01 01:18:01 +0800

    Update custom.sh

--- files/etc/uci-defaults/custom.sh
+++ files/etc/uci-defaults/custom.sh
@@ -3,12 +3,6 @@
 # 设置默认防火墙规则，方便虚拟机首次访问 WebUI
 uci set firewall.@zone[1].input='ACCEPT'
 
-# 设置主机名映射，解决安卓原生 TV 无法联网的问题
-uci add dhcp domain
-uci set "dhcp.@domain[-1].name=time.android.com"
-uci set "dhcp.@domain[-1].ip=203.107.6.88"
-
-
 # 根据网卡数量配置网络
 count=0
 for iface in /sys/class/net/*; do
